4e2725fde1b2db3bc33c2930625209a42cb48d2a
baileyshelden <bailey7748@gmail.com>
2018-06-08 16:11:32 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,20 +1,20 @@
 # Write your code here
 
 
-dollar = gets.chomp
+# dollar = gets.chomp
 
-def usd_to_eur 
+# def usd_to_eur 
 
-puts "How many United States Dollars do you have?"
-dollar = gets.chomp.to_i
+# puts "How many United States Dollars do you have?"
+# dollar = gets.chomp.to_i
  
-eur = dollar * 0.85
+# eur = dollar * 0.85
  
-puts "You now have #{eur.round(2)} Euros."
-end 
+# puts "You now have #{eur.round(2)} Euros."
+# end 
 
 
-usd_to_eur
+# usd_to_eur
 
 
 # method 2 
