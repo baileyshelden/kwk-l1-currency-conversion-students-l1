74eed74558ec2e2572d0a7277e54549ce2cfd64f
baileyshelden <bailey7748@gmail.com>
2018-06-08 16:06:57 +0000

    Done.

--- currency.rb
+++ currency.rb
@@ -1,1 +1,67 @@
-# Write your cod here
+# Write your code here
+
+
+dollar = gets.chomp
+
+def usd_to_eur 
+
+puts "How many United States Dollars do you have?"
+dollar = gets.chomp.to_i
+ 
+eur = dollar * 0.85
+ 
+puts "You now have #{eur.round(2)} Euros."
+end 
+
+
+usd_to_eur
+
+
+# method 2 
+def eur_to_usd
+
+puts "How many Euros do you have?"
+eur = gets.chomp.to_i
+ 
+dollar = eur * 1.18
+ 
+puts "You now have #{dollar.round(2)} Euros."
+end 
+
+
+eur_to_usd
+
+
+# challenge 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
